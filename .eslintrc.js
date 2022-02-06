5849e3454a36f94943de55c8fd78b60038df7ee7
João Palmeiro <joaommpalmeiro@gmail.com>
2022-02-06 16:03:41 +0000

    Update .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:import/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2021,
+    sourceType: "module",
+  },
+  rules: {
+    eqeqeq: "error",
+    "no-else-return": "error",
+    "no-console": "warn",
+    "import/order": [
+      "warn",
+      {
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+        warnOnUnassignedImports: true,
+      },
+    ],
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+    "import/resolver": {
+      // https://github.com/import-js/eslint-plugin-import#settings
+      // https://github.com/import-js/eslint-plugin-import/issues/869#issuecomment-320267555
+      // https://www.npmjs.com/package/eslint-import-resolver-node
+      // https://github.com/import-js/eslint-plugin-import#resolvers
+      node: {
+        extensions: [".js", ".jsx"],
+      },
+    },
+  },
+};
